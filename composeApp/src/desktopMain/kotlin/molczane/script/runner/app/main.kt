b0604b7a9f7559bc9bcc5d172c661495996db9ed
molczane <ernest.molczan@gmail.com>
2024-11-02 16:48:27 +0100

    refactor: Centralize script stopping logic in ScriptViewModel
    This commit refactors the script stopping logic to be handled centrally by the `ScriptViewModel`.
    
    Previously, the `onCloseRequest` handler in the `main` function manually terminated running processes. This commit moves that logic into the `ScriptViewModel.stopScript()` function, providing a more organized and maintainable approach.

--- composeApp/src/desktopMain/kotlin/molczane/script/runner/app/main.kt
+++ composeApp/src/desktopMain/kotlin/molczane/script/runner/app/main.kt
@@ -5,19 +5,13 @@ import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
-import molczane.script.runner.app.utils.killProcess
 import molczane.script.runner.app.viewModel.ScriptViewModel
 
 fun main() = application {
     val viewModel = remember { ScriptViewModel() }
     Window(
         onCloseRequest = {
-            // Terminate all running processes
-            viewModel.processList.forEach { process ->
-                if (process.isAlive) {
-                    killProcess(process)
-                }
-            }
+            viewModel.stopScript()
 
             try {
                 viewModel.fileToDestroy!!.delete()

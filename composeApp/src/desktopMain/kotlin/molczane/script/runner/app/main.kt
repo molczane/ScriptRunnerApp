96f07134bfc245fc31d96fa6a56b62bf473eb32f
molczane <ernest.molczan@gmail.com>
2024-11-04 23:29:32 +0100

    refactor: Remove unused code and simplify main function
    This commit removes unused code and simplifies the main function:
    
    - Removed unused code for loading and setting the application icon.
    - Removed unused `runScript` and `stopScript` functions from the `ScriptViewModel`.
    - This change cleans up the codebase and removes unnecessary elements.

--- composeApp/src/desktopMain/kotlin/molczane/script/runner/app/main.kt
+++ composeApp/src/desktopMain/kotlin/molczane/script/runner/app/main.kt
@@ -6,15 +6,9 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import molczane.script.runner.app.viewModel.ScriptViewModel
-import java.awt.Image
-import javax.imageio.ImageIO
 
 fun main() = application {
     val viewModel = remember { ScriptViewModel() }
-
-    // To be deleted
-    val iconStream = this::class.java.getResourceAsStream("/icons/AppIcon.icns")
-    val iconImage: Image? = iconStream?.let { ImageIO.read(it) }
 
     Window(
         onCloseRequest = {
@@ -33,10 +27,6 @@ fun main() = application {
         title = "ScriptRunnerApp",
         state = WindowState(size = DpSize.Unspecified)
     ) {
-        if (iconImage != null) {
-            window.iconImage = iconImage // Assign the loaded icon image
-        }
-
         App(viewModel)
     }
 }
